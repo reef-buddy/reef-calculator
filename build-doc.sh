0f1ca4f138c22e69f241b6ffe8cf41c3159a260d
Maxime Marinel <bourvill@users.noreply.github.com>
2022-05-04 08:06:50 +0200

    Add link to documentation

--- build-doc.sh
+++ build-doc.sh
@@ -1,3 +1,10 @@
 #!/bin/zsh
 
-swift package --allow-writing-to-directory ./docs generate-documentation --target ReefCalculator --disable-indexing --transform-for-static-hosting --output-path ./docs --hosting-base-path reef-calculator
+swift package \
+--allow-writing-to-directory ./docs \
+generate-documentation \
+--target ReefCalculator \
+--disable-indexing \
+--transform-for-static-hosting \
+--output-path ./docs \
+--hosting-base-path reef-calculator
